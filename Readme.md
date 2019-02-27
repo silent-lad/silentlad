9c1abee1424c88ff6583631a477ab78b80c6874c
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-02-27 13:00:58 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# SilentLad
+
+This is my resume..Please don't read it.
+
+## Installation
+
+- Run `npm i --global silentlad`.
+- Then in terminal run `silentlad`.
+- Baby Shark todo dodo dodo.
---
+++ index.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+"use strict";
+
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+var response = chalk.bold.green;
+
+var resume = require("./resume.json");
+
+var resumePrompts = {
+  type: "list",
+  name: "resumeOptions",
+  message: "What do you want to know about me?",
+  choices: [...Object.keys(resume), "Exit"]
+};
+
+function main() {
+  console.log("Hello,My name is SilentLad and welcome to my resume");
+  resumeHandler();
+}
+
+function resumeHandler() {
+  inquirer.prompt(resumePrompts).then(answer => {
+    if (answer.resumeOptions == "Exit") {
+      return;
+    }
+    var option = answer.resumeOptions;
+    console.log(response("--------------------------------------"));
+    resume[`${option}`].forEach(info => {
+      console.log(response("|   => " + info));
+    });
+    console.log(response("--------------------------------------"));
+    // console.log(resume[`${option}`]);
+    inquirer
+      .prompt({
+        type: "list",
+        name: "exitBack",
+        message: "Go back or Exit?",
+        choices: ["Go Back", "Exit"]
+      })
+      .then(choice => {
+        if (choice.choices == "Go Back") {
+          resumeHandler();
+        } else {
+          return;
+        }
+      });
+  });
+}
+
+main();
